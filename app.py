b398a87280ce1df2035883aa0ce8c20f22375526
anish2454 <shenoy.anish.227@gmail.com>
2017-12-02 17:50:05 -0500

    fefwe

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, session, flash, request
 import os
 import json
 import urllib2
+import urllib
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
@@ -19,19 +20,27 @@ def route():
     destinations = ""
     b=bathrooms()
     if request.args.get("address"):
-        #origins = request.args.get("address")
-        origins = "Washington,DC"
+        origins = urllib.quote(request.args.get("address"))
         count = 0
         for bathroom in b:
-            if(count < 80):
+            if(count < 100):
                 destinations += ("place_id:" + bathroom["place_id"] + "|")
+                count+=1
         destinations = destinations[:-1]
         url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + origins + "&destinations=" + destinations + "&key=" + matrix_key
         print url
         uResp = urllib2.urlopen(url)
         raw = uResp.read()
         dat = json.loads(raw)
-        print dat
+        min = dat["rows"][0]["elements"][0]["distance"]["value"]
+        count = 0
+        minindex = 0
+        for element in dat["rows"][0]["elements"]:
+            count+=1
+            if("distance" in element and element["distance"]["value"] < min):
+                min = element["distance"]["value"]
+                minindex = count
+        print b[minindex]
     return render_template("test.html")
 
 
